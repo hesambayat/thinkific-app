6916e1d882d3f253f3da6a82d122d8eb46f3608a
Hesam Bayat <hesam.bayat@gmail.com>
2019-11-20 16:20:38 +0400

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useUser } from './context/user-context'
+import * as Pages from './pages'
 
-function App() {
+const AuthenticatedRoutes = () => (
+  <Switch>
+    <Route path="/" exact component={Pages.Dashboard} />
+    <Route path="/edit/:courseId" component={Pages.EditCourse} />
+    <Route component={Pages.NotFound} />
+  </Switch>
+)
+
+const UnauthenticatedRoutes = () => (
+  <Route path="/" component={Pages.Login} />
+)
+
+export default () => {
+  const { user } = useUser()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      {user ? <AuthenticatedRoutes /> : <UnauthenticatedRoutes />}
+    </Router>
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { AppProviders } from './context'
+import { Client } from './lib'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
+import './style'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
